f988ddc6f85ad37b41cc97278c047fc9fb1cac18
A-18 <A-18@A-18>
2024-04-29 09:51:00 +0700

    halo

---
+++ latihan5/src/main/java/com/mycompany/latihan5/lat5.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan5;
+
+/**
+ *
+ * @author A-18
+ */
+public class lat5 {
+    public static void main(String[] args) {
+        System.out.println("halo");
+    }
+}
